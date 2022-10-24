6d28eba66b10d4511b80f240ffae740cd6ee3942
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-10-24 13:17:37 +0100

    refactors code

--- core/models.py
+++ core/models.py
@@ -1,69 +1,14 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
-from core.choices import theme_choices
-from django.conf import settings
-
 from posts.models import Post
 
-UserModel = settings.AUTH_USER_MODEL
+from core.choices import theme_choices
+from core.managers import UserManager
+
+UserModel = get_user_model()
 
 # Create your models here.
-
-
-class UserManager(BaseUserManager):
-    def create_user(
-        self,
-        email,
-        first_name,
-        password=None,
-        **extra_fields,
-    ):
-        """Method for creating user"""
-        if not email:
-            raise ValueError("Email is Required")
-        if not first_name:
-            raise ValueError("First Name is Required")
-
-        email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_staffuser(self, email, first_name, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", False)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Staff user must have is_staff=True")
-
-        user = self.create_user(
-            email=email,
-            first_name=first_name,
-            password=password,
-            **extra_fields,
-        )
-        return user
-
-    def create_superuser(self, email, first_name, password, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_admin", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Staff user must have is_staff=True")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True")
-
-        user = self.create_user(
-            email=email,
-            first_name=first_name,
-            password=password,
-            **extra_fields,
-        )
-        return user
 
 
 class User(AbstractBaseUser):
@@ -96,6 +41,7 @@ class User(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
+    # TODO: this should be handled as session/cookie on the frontend
     theme = models.CharField(max_length=15, choices=theme_choices, default="dark")
     friends = models.ManyToManyField(
         UserModel,
@@ -111,14 +57,11 @@ class User(AbstractBaseUser):
 
     @property
     def full_name(self):
-        if len(self.last_name) > 0 and len(self.middle_name) > 0:
-            return f"{self.first_name} ({self.middle_name}) {self.last_name}"
-        elif len(self.last_name) <= 0 and len(self.middle_name) > 0:
-            return f"{self.first_name} ({self.middle_name})"
-        elif len(self.last_name) > 0 and len(self.middle_name) <= 0:
-            return f"{self.first_name} {self.last_name}"
-        else:
-            return f"{self.first_name}"
+        first_name = self.first_name or ""
+        middle_name = self.middle_name or ""
+        last_name = self.last_name or ""
+
+        return f"{first_name} {middle_name} {last_name}"
 
     @property
     def posts(self):
@@ -127,16 +70,6 @@ class User(AbstractBaseUser):
 
     def get_short_name(self):
         return self.first_name
-
-    # Permissions
-    def has_perms(self, perm, obj=None):
-        return True
-
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
 
     objects = UserManager()
 
